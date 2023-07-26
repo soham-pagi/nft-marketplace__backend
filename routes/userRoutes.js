b30ab2c103e2d0c6e68b9413894b57dd5042315a
Soham Pagi <paguisoham36@gmail.com>
2023-07-26 17:00:02 +0530

    Remove fs module dependency

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const multer = require("multer");
 const userController = require("../controllers/userController");
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer().single("image");
 
 // Create or update a user
-router.post("/", upload.single("image"), userController.createUser);
+router.post("/", upload, userController.createUser);
 
 // Get user by wallet address
 router.get("/:metamaskWalletAddress", userController.getUserByWalletAddress);
